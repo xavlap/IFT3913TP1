fe2785a6d35e86f49f0c72bada71982715b797e4
labrassard <brassard496@gmail.com>
2022-09-20 16:42:59 -0400

    start #2

--- jls.py
+++ jls.py
@@ -1,21 +1,7 @@
-import csv
-import os.path
-import sys
+import os
 
+input = input("please enter a directory : ")
 
-def dir_is_dir(dir_path):
-
-    if os.path.isdir(dir_path):
-        jls(dir_path)
-    else:
-        print("The path provided does not lead to a directory")
-
-
-def jls(dirPath):
-    file_to_write = open("/home/louis/Documents/Ecole/Session6/IFT3913/IFT3913TP1",'w')
-    writer = csv.writer(file_to_write)
-    writer.writerow('row')
-    file_to_write.close()
-
-
-dir_is_dir(sys.argv[1])
+for root, dirs, files in os.walk(input,):
+    for name in files:
+        print("{path}, {package}, {file}".format(path = os.path.join(root, name),package = root.replace("/","."),file = name))
