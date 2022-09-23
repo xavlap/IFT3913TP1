4777dbfc68904bea1fd03428bf4e763f42f7b1ce
xavier lapalme <xavier.lapalme@umontreal.cayesgit config --global user.name xavier>
2022-09-23 19:38:40 -0400

    phase2 pas efficace donc a travaille

--- jls.py
+++ jls.py
@@ -1,13 +1,11 @@
+import sys
 import os
 import csv
 
 f = open(os.getcwd() + "/myCSV.csv","w")
 writer = csv.writer(f)
 
-
-input = input("please enter a directory : ")
-
-for root, dirs, files in os.walk(input): 
+for root, dirs, files in os.walk(sys.argv[1]): 
     for name in files:
         writer.writerow([os.path.join(root, name),root.replace("/","."), name])
 
