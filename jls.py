7a708c684766f84e853f261e44305d538aa379ea
labrassard <brassard496@gmail.com>
2022-09-25 14:49:26 -0400

    ..

--- jls.py
+++ jls.py
@@ -6,14 +6,12 @@ import csv
 def jls(file_path):
     f = open(os.getcwd() + "/myCSV.csv", "w")
     writer = csv.writer(f)
-    print(os.getcwd())
 
     for root, dirs, files in os.walk(file_path):
         for name in files:
             class_name = name.replace(".java", "")
             file_location = root.replace(sys.argv[1], ".")
-            package_location = root.replace(sys.argv[1]+"/", "").replace("/", ".")
-            print(package_location)
+            package_location = root.replace(sys.argv[1] + "/", "").replace("/", ".")
             writer.writerow([os.path.join(file_location, name), package_location, class_name])
     f.close()
 
